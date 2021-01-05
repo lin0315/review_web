e6b6f4a0b46549ce6756107821dbf8af58f8e2e0
lin <1845948316@qq.com>
2021-01-05 21:53:29 +0800

    Vuex_2021/01/05

--- Vue/Vuex/vuex-test/src/App.vue
+++ Vue/Vuex/vuex-test/src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
+    <div>--------app组件-----</div>
     <button @click="additionClick">+</button>
     <button @click="subtractClcik">+</button>
+    <button @click="addcount(5)">+5</button>
+    <button @click="addcount(10)">+10</button>
+    <button @click="addstudent">添加学生</button>
+
+    <div>------app组件--getters</div>
+    <div>平方:{{ $store.getters.powerCount }}</div>
+    <div>学生年龄大于50: {{ $store.getters.more50stu }}</div>
+    <div>学生年龄大于50的人数: {{ $store.getters.more50stulength }}</div>
+    <div>传递参数age>40:{{ $store.getters.moreAgeStu(40) }}</div>
+
     <div>-------Home组件-------</div>
     <Home></Home>
     <div>-------HelloVuex组件-------</div>
@@ -24,6 +35,22 @@ export default {
     },
     subtractClcik() {
       this.$store.commit("idecrement");
+    },
+    // 1普通提交风格
+    // addcount(count) {
+    //   this.$store.commit("incrementCount", count);
+    // },
+
+    // 2.特殊的提交风格
+    addcount(count) {
+      this.$store.commit({
+        type: "incrementCount",
+        count,
+      });
+    },
+    addstudent() {
+      const stu = { name: "孙七", age: 77 };
+      this.$store.commit("addStudent", stu);
     },
   },
 };
--- Vue/Vuex/vuex-test/src/store/index.js
+++ Vue/Vuex/vuex-test/src/store/index.js
@@ -4,17 +4,61 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // state: 类似于Vue实例里面的data
   state: {
-    count: 10
+    count: 10,
+    students: [
+      { name: '张三', age: 33 },
+      { name: '李四', age: 44 },
+      { name: '王五', age: 55 },
+      { naem: '赵六', age: 66 }
+    ]
   },
+  // mutations: 类似于Vue实例里面的methods
   mutations: {
     increment(state) {
       state.count++
     },
     idecrement(state) {
       state.count--
+    },
+    // 1普通提交风格
+    // incrementCount(state, count) {
+    //   state.count += count
+    // },
+
+    // 2特殊提交风格
+    incrementCount(state, payload) {
+      state.count += payload.count
+    },
+    addStudent(state, stu) {
+      state.students.push(stu)
+    }
+
+  },
+  // getters 类似于Vue实例的computed
+  // 参数：第一个为state 第二个为getters 注意没有第三个
+  getters: {
+    // 获取count的平方
+    powerCount(state) {
+      return state.count * state.count
+    },
+    // 获取students中age大于50的学生
+    more50stu(state) {
+      return state.students.filter(item => item.age > 50)
+    },
+    // 获取students中age大于50的学生人数
+    more50stulength(state, getters) {
+      return getters.more50stu.length
+    },
+    // 需要另外参数时 
+    moreAgeStu(state) {
+      return function (age) {
+        return state.students.filter(item => item.age > age)
+      }
     }
   },
+
   actions: {
   },
   modules: {
