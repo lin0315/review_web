f8e4fb4092bd48860acc0c545ec8d1609425f667
lin <1845948316@qq.com>
2021-01-07 22:03:59 +0800

    Vuex_2021/01/07

---
+++ JS/深拷贝/copytest01.js
@@ -0,0 +1,31 @@
+function deepCopy(obj = {}) {
+  // 判断 如果是值类型 则直接返回
+  if (typeof obj !== 'Object' || obj == null) {
+    return obj
+  }
+  //  初始化结果
+  let result
+  if (obj instanceof Array) {
+    result = []
+  } else {
+    result = {}
+  }
+  for (let key in obj) {
+    if (obj.hasOwnproperty(key)) {
+      result[key] = deepCopy(obj[key])
+    }
+  }
+  return result;
+}
+
+let a = {
+  name: '张三',
+  age: 20,
+  hobby: {
+    hoby1: '打游戏',
+    hoby2: '游泳'
+  }
+}
+let b
+b = deepCopy(a)
+console.log(b);
